dba65b5f881c8be6c7e099ae03e74c1d53a9cf0e
jeter <b05705054@ntu.edu.tw>
2019-01-12 15:41:06 +0800

    gihjl;

--- word/models.py
+++ word/models.py
@@ -3,19 +3,18 @@ from django.utils.timezone import now
 from django.contrib import admin
 from django.conf import settings
 # Create your models here.
-class Word(models.Model):
-	name = models.CharField(max_length=64,blank = True)
+
 class Category(models.Model):
 	catDef = models.CharField(max_length=64,blank = True)
 	
 
-<<<<<<< HEAD
 
-=======
+
+
 class Word(models.Model):
 	name = models.CharField(max_length=64,blank = True)
 	cat = models.OneToOneField(Category,on_delete=models.CASCADE,primary_key=False)
->>>>>>> 267d1423b31615286347ea78150c95188c31a144
+
 	
 
 	def __str__(self):
