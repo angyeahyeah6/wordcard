b98a5af9a81e496d38551b53738353fd80b5bc06
Angela <b05705039@ntu.edu.tw>
2019-01-13 11:30:24 +0800

    more on account

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name='account'
 
 urlpatterns = [
-	path('login/',views.login,name = 'login'),
+	path('login/',views.login_view,name = 'login'),
     path('register/', views.register,name="register"),
     path('logout/', views.logout_view,name = 'logout'),
 ]
--- account/views.py
+++ account/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 
-def login(request):
+def login_view(request):
 	if request.method == 'POST':
 		form = AuthenticationForm(data=request.POST)
 		if form.is_valid():
