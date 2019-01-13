16cd04c0f716c8014e69f3dff1c4462dc1d19fa9
Angela <b05705039@ntu.edu.tw>
2019-01-13 11:51:25 +0800

    required

--- word/views.py
+++ word/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render,redirect   # 加入 redirect 套件
 from django.contrib.auth import authenticate
-from .models import Word, Phrase
+from .models import Word, Phrase, Relate
 import simplejson
 import json
 from django.http import JsonResponse
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/account/register/')
 def index(request):
 	if "create_word" in request.POST:
 		word = request.POST['word_name']
@@ -19,17 +20,27 @@ def index(request):
 		word.append(i.name)
 		definition.append(i.definition)
 	return render(request,"index.html",locals())
-
+@login_required(login_url='/account/register/')
 def phrase(request):
+	word_relate = []
+	phrases = ""
 	if "create_phrase" in request.POST:
 		phrases = request.POST['phrase_name']
 		definition = request.POST['phrase_def']
-		Phrase.objects.create(name=phrases, phraseDef=definition)
-	
+		phrase = Phrase.objects.create(name=phrases, phraseDef=definition)
+		word_relate = phrases.split(" ")
+		for i in word_relate:
+			if len(i) > 5:
+				try:
+					word = Word.objects.get(name=i)
+				except:
+					word = Word.objects.create(name=i,definition="")
+				Relate.objects.create(word=word,phrase=phrase)
 	all_phrase = Phrase.objects.all()
 	phrases = []
 	definition = []
 	for i in all_phrase:
 		phrases.append(i.name)
 		definition.append(i.phraseDef)
+
 	return render(request,"phrase.html",locals())
